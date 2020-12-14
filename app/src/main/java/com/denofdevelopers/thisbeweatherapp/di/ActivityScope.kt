366fa07cb8c1b6165c4e187e99164f56d66396a0
Aleksandar Gulevski <agulevski10@gmail.com>
2020-12-14 19:51:12 +0100

    Basic project setup - added di package, ApiModule and ActivityScope

---
+++ app/src/main/java/com/denofdevelopers/thisbeweatherapp/di/ActivityScope.kt
@@ -0,0 +1,7 @@
+package com.denofdevelopers.thisbeweatherapp.di
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityScope
---
+++ app/src/main/java/com/denofdevelopers/thisbeweatherapp/di/modules/ApiModule.kt
@@ -0,0 +1,57 @@
+package com.denofdevelopers.thisbeweatherapp.di.modules
+
+import com.denofdevelopers.thisbeweatherapp.BuildConfig
+import com.denofdevelopers.thisbeweatherapp.network.ApiService
+import com.google.gson.Gson
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+class ApiModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(httpClient: OkHttpClient, gson: Gson): Retrofit {
+        return Retrofit.Builder()
+            .client(httpClient)
+            .baseUrl(BuildConfig.API_BASE_URL)
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+
+    @Provides
+    fun provideHttpClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        builder.readTimeout(100, TimeUnit.SECONDS)
+        builder.connectTimeout(100, TimeUnit.SECONDS)
+        builder.retryOnConnectionFailure(true)
+        if (BuildConfig.DEBUG) {
+            val logging = HttpLoggingInterceptor();
+            logging.apply { logging.level = HttpLoggingInterceptor.Level.BODY }
+            builder.addInterceptor(logging)
+        }
+        return builder.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create<ApiService>(ApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return Gson()
+    }
+}
