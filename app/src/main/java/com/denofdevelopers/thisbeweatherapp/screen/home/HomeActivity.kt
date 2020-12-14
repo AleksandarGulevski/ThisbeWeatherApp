13cf933ebff59c7a57f33b75bf3b388e2f2e084e
Aleksandar Gulevski <agulevski10@gmail.com>
2020-12-14 20:49:57 +0100

    Renamed MainActivity to HomeActivity and added starter method

--- app/src/main/java/com/denofdevelopers/thisbeweatherapp/screen/home/HomeActivity.kt
+++ app/src/main/java/com/denofdevelopers/thisbeweatherapp/screen/home/HomeActivity.kt
@@ -1,11 +1,20 @@
-package com.denofdevelopers.thisbeweatherapp
+package com.denofdevelopers.thisbeweatherapp.screen.home
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.denofdevelopers.thisbeweatherapp.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
+    }
+
+    companion object{
+        fun start(context: Context){
+            context.startActivity(Intent(context, HomeActivity::class.java))
+        }
     }
 }
