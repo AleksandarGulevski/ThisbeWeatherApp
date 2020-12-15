33a53a300ed9d0e7480d18a17cc2322d8ca525b8
Aleksandar Gulevski <agulevski10@gmail.com>
2020-12-15 22:59:01 +0100

    Added ApiService methods to get weather by city and by device location

--- app/src/main/java/com/denofdevelopers/thisbeweatherapp/network/ApiService.kt
+++ app/src/main/java/com/denofdevelopers/thisbeweatherapp/network/ApiService.kt
@@ -1,4 +1,25 @@
 package com.denofdevelopers.thisbeweatherapp.network
 
+import com.denofdevelopers.thisbeweatherapp.model.WeatherResponse
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
+
+    @GET("weather")
+    fun getWeatherByCity(
+        @Query("q") city: String,
+        @Query("APPID") API_KEY: String,
+        @Query("units") UNITS: String
+    ): Single<WeatherResponse>
+
+
+    @GET("weather")
+    fun getWeatherByDeviceLocation(
+        @Query("lat") latitude: Double,
+        @Query("lon") longitude: Double,
+        @Query("APPID") API_KEY: String,
+        @Query("units") UNITS: String
+    ): Single<WeatherResponse>
 }
