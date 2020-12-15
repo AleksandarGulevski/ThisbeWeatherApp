92874cc422a05c2b29530aed868222476eaf8c00
Aleksandar Gulevski <agulevski10@gmail.com>
2020-12-15 20:51:43 +0100

    Added method to show date and time

--- app/src/main/java/com/denofdevelopers/thisbeweatherapp/screen/home/HomeActivity.kt
+++ app/src/main/java/com/denofdevelopers/thisbeweatherapp/screen/home/HomeActivity.kt
@@ -9,17 +9,25 @@ import com.denofdevelopers.thisbeweatherapp.application.App
 import com.denofdevelopers.thisbeweatherapp.common.BaseActivity
 import com.denofdevelopers.thisbeweatherapp.util.MessageUtil.toast
 import kotlinx.android.synthetic.main.activity_home.*
+import java.text.DateFormat
+import java.util.*
 
 class HomeActivity : BaseActivity(), HomeContract.View {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        showDateAndTime()
     }
 
     companion object {
         fun start(context: Context) {
             context.startActivity(Intent(context, HomeActivity::class.java))
         }
+    }
+
+    private fun showDateAndTime() {
+        dateTime.text = DateFormat.getDateTimeInstance().format(Date());
     }
 
     override fun showProgress() {
