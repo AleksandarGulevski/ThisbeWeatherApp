e86d39fd4723e2d0c3ab0af1562fcddc5eb1dbac
Aleksandar Gulevski <agulevski10@gmail.com>
2020-12-15 23:01:02 +0100

    Implemented api calls and response handling

--- app/src/main/java/com/denofdevelopers/thisbeweatherapp/screen/home/HomePresenter.kt
+++ app/src/main/java/com/denofdevelopers/thisbeweatherapp/screen/home/HomePresenter.kt
@@ -1,26 +1,61 @@
 package com.denofdevelopers.thisbeweatherapp.screen.home
 
+import com.denofdevelopers.thisbeweatherapp.R
+import com.denofdevelopers.thisbeweatherapp.common.Constants.API_KEY
+import com.denofdevelopers.thisbeweatherapp.common.Constants.UNITS
+import com.denofdevelopers.thisbeweatherapp.model.WeatherResponse
 import com.denofdevelopers.thisbeweatherapp.network.ApiService
+import com.denofdevelopers.thisbeweatherapp.util.NetworkUtil
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 
 class HomePresenter(
     private var activity: HomeActivity,
     private var apiService: ApiService
 ): HomeContract.Presenter {
 
+    private var request: Disposable? = null
+
+    override fun onStart() {}
+
     override fun getWeatherByCity(city: String) {
-        TODO("Not yet implemented")
+        activity.showProgress()
+        request = apiService.getWeatherByCity(city, API_KEY, UNITS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this::onGetWeatherSuccess, this::onError)
     }
 
     override fun getWeatherByDeviceLocation(latitude: Double, longitude: Double) {
-        TODO("Not yet implemented")
+        activity.showProgress()
+        request = apiService.getWeatherByDeviceLocation(latitude, longitude, API_KEY, UNITS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this::onGetWeatherSuccess, this::onError)
     }
 
-    override fun onStart() {
-        TODO("Not yet implemented")
+    private fun onGetWeatherSuccess(weatherResponse: WeatherResponse) {
+        //TODO create method in activity to display the results
+        activity.hideProgress()
+    }
+
+    private fun onError(error: Throwable) {
+        activity.hideProgress()
+        if (!NetworkUtil.isInternetAvailable(activity)) {
+            activity.showMessage(activity.getString(R.string.no_internet))
+        } else {
+            activity.showMessage(activity.getString(R.string.generic_error))
+            Timber.e(error, "Home presenter error")
+        }
     }
 
     override fun onStop() {
-        TODO("Not yet implemented")
+        cancelRequest()
     }
 
+    private fun cancelRequest() {
+        request?.dispose()
+    }
 }
